770f9d440f5327ee95b9681a2c17128942b83bb2
Dante Deslauriers <64750637+ddesl069@users.noreply.github.com>
2024-03-08 22:58:45 -0500

    Update generalized_dynamic_structure.py
    added comments

--- generalized_dynamic_structure.py
+++ generalized_dynamic_structure.py
@@ -17,6 +17,8 @@ bboxes_person_img1: dict[BoundingBox, str] = {
     (463,251,112,206): 'girl in brown vest 2',
 }
 
+""" Used the calculate histogram function from opencv, calculates the histogram of the bbox
+given, it also normalizes the histogram as well """
 def calculate_histogram(img: MatLike, bbox: BoundingBox, mask_type: str) -> Histogram:
     mask = np.zeros(img.shape[:2], np.uint8)
     x, y, w, h = bbox
@@ -30,7 +32,8 @@ def calculate_histogram(img: MatLike, bbox: BoundingBox, mask_type: str) -> Hist
     normalized_hist = (hist / hist.sum()) * 100
     
     return normalized_hist
-
+""" calculate correlation function taken from opencv that finds the correlation of two histograms
+the higher the correlation the more accurate it is supposed to be"""
 def calculate_correlation(hist1: Histogram, hist2: Histogram) -> float:
     return cv.compareHist(hist1, hist2, cv.HISTCMP_CORREL)
 
